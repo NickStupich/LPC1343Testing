34269d3c4d033594eb45bf4b83785c010279f0b4
Nick Stupich <nick.stupich@gmail.com>
2012-06-19 18:35:57 -0400

    trying to get gpio interrupts working, found and fixed some potential bugs but still not working in the simulator

--- Test2/main.c
+++ Test2/main.c
@@ -1,5 +1,6 @@
 #include "lpc13xx.h"
 #include "lpc1343.h"	//my own definitions
+
 int main()
 {
 	
@@ -9,21 +10,30 @@ int main()
 
 void SystemInit()
 {
-	//set up GPIO0 for external interrupts
 	unsigned int bitPos = 1;
+	
+	__disable_irq();
+	//set up GPIO0 for external interrupts
 	
 	SCB_SYSAHBCLKCTRL |= (SCB_SYSAHBCLKCTRL_GPIO);	//give gpio a clock
 	SCB_SYSAHBCLKCTRL |= (SCB_SYSAHBCLKCTRL_IOCON);	//iocon gets a clock
 	
+	LPC_GPIO0->DIR &= ~(1<<bitPos);						//input
 	LPC_GPIO0->IS &= ~( 1<< bitPos);					//edge sensitive
 	LPC_GPIO0->IBE &= ~(1<<bitPos);						//single edge
 	LPC_GPIO0->IEV &= ~(1<<bitPos);						//falling edge sensitive
 	LPC_GPIO0->IE |= (1<<bitPos);							//un-mask interrupt
 	
-	ISER1 |= 1<<24;	//enable interrupt for GPIO 0
+	//ISER1 |= 1<<24;	//enable interrupt for GPIO 0
+	NVIC_EnableIRQ(EINT0_IRQn);
+	NVIC_SetPriority(EINT0_IRQn, 0x1F);
 	
-	IPR14 |= (0x1F << 3);//highest priority for P0_1
+	//IPR14 |= (0x1F << 3);//highest priority for P0_1
 	LPC_IOCON->PIO0_1 = 0x10;		//pull-up, no hysterisis, GPIO function
+	
+	
+	
+	__enable_irq();
 }
 
 void PIOINT0_IRQHandler(void)
