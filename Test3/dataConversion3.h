2d3cf7529b05d2792d6980f0b0c78d1743a15e54
Nick Stupich <nick.stupich@gmail.com>
2012-06-22 09:51:32 -0400

    test was unfair, dataConversion3.h is fastest because it was converting the status byte while the others were not

--- Test3/dataConversion3.h
+++ Test3/dataConversion3.h
@@ -23,16 +23,23 @@ typedef union{
 
 
 channelUnion cu[9];
-void testDataConversion()	//182 cycles
+void testDataConversion()	//172 cycles
 {	
+	//int i;
 	unsigned char* dataPtr;
 	channelUnion* cuPtr;
+	//int (*cdPtr)[100];
 	cuPtr	= &cu[0];
 	dataPtr = &_rawData[0];
+	
 		
-	cuPtr->raw.bytes[3] = *(dataPtr++);
-	cuPtr->raw.bytes[2] = *(dataPtr++);
-	(*(cuPtr++)).raw.bytes[1] = *(dataPtr++);
+	/*	
+	for(i=0;i<9;i++)		//245 cycles using the loop
+	{
+		cuPtr->raw.bytes[3] = *(dataPtr++);
+		cuPtr->raw.bytes[2] = *(dataPtr++);
+		(*(cuPtr++)).raw.bytes[1] = *(dataPtr++);
+	}*/
 	
 	cuPtr->raw.bytes[3] = *(dataPtr++);
 	cuPtr->raw.bytes[2] = *(dataPtr++);
@@ -65,6 +72,24 @@ void testDataConversion()	//182 cycles
 	cuPtr->raw.bytes[3] = *(dataPtr++);
 	cuPtr->raw.bytes[2] = *(dataPtr++);
 	(*(cuPtr++)).raw.bytes[1] = *(dataPtr++);
+	
+	cuPtr->raw.bytes[3] = *(dataPtr++);
+	cuPtr->raw.bytes[2] = *(dataPtr++);
+	(*(cuPtr++)).raw.bytes[1] = *(dataPtr++);
+	
+	/*
+	cdPtr = (int (*)[100])&channelData[8][channelIndex];
+	*((int *)cdPtr--) = SCALE_INTEGER((*(--cuPtr)).value);
+	*((int *)cdPtr--) = SCALE_INTEGER((*(--cuPtr)).value);
+	*((int *)cdPtr--) = SCALE_INTEGER((*(--cuPtr)).value);
+	*((int *)cdPtr--) = SCALE_INTEGER((*(--cuPtr)).value);
+	*((int *)cdPtr--) = SCALE_INTEGER((*(--cuPtr)).value);
+	*((int *)cdPtr--) = SCALE_INTEGER((*(--cuPtr)).value);
+	*((int *)cdPtr--) = SCALE_INTEGER((*(--cuPtr)).value);
+	*((int *)cdPtr--) = SCALE_INTEGER((*(--cuPtr)).value);
+	*/
+	//*((int* )cdPtr) = SCALE_INTEGER((*(--cuPtr)).value);
+	//*((int* )(cdPtr-= 100*sizeof(int))) = SCALE_INTEGER((*(--cuPtr)).value);
 	
 	channelData[8][channelIndex] = SCALE_INTEGER((*(--cuPtr)).value);
 	channelData[7][channelIndex] = SCALE_INTEGER((*(--cuPtr)).value);
@@ -74,6 +99,6 @@ void testDataConversion()	//182 cycles
 	channelData[3][channelIndex] = SCALE_INTEGER((*(--cuPtr)).value);
 	channelData[2][channelIndex] = SCALE_INTEGER((*(--cuPtr)).value);
 	channelData[1][channelIndex] = SCALE_INTEGER((*(--cuPtr)).value);
-	channelData[0][channelIndex] = SCALE_INTEGER((*(--cuPtr)).value);
+	//channelData[0][channelIndex] = SCALE_INTEGER((*(--cuPtr)).value);
 	
 }
